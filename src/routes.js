6338452990c9665fc5f7f1fb43983e7e72028416
jaemin.han <jaemin.han@nyu.edu>
2018-07-24 17:06:26 -0400

    create and implement form inputs

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from './components/App';
 import HomePage from './components/Home/HomePage';
 import AboutPage from './components/About/AboutPage';
 import CoursesPage from './components/Course/CoursesPage';
+import ManageCoursePage from './components/Course/ManageCoursePage';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
         <Route path="about" component={AboutPage}/>
         <Route path="courses" component={CoursesPage}/>
+        <Route path="course" component={ManageCoursePage}/>
+        <Route path="course/:id" component={ManageCoursePage}/>
     </Route>
 );
