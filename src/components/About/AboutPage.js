c66ad729a1f6f45e5b4d8d90dab177c6413512f2
jaemin.han <jaemin.han@nyu.edu>
2018-07-23 22:49:28 -0400

    route foundation complete

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 class AboutPage extends React.Component {
     render() {
         return (
-            <div className="jumbotron">
+            <div>
                 <h1>About Page</h1>
                 <p>This app uses React, Redux, React Router, and more!</p>
             </div>
