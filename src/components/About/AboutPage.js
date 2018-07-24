6122a1c6a66a5d61334eddd71ddab7a1dc877353
jaemin.han <jaemin.han@nyu.edu>
2018-07-23 22:23:54 -0400

    app routes setup and basic nav complete

---
+++ src/components/About/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>About Page</h1>
+                <p>This app uses React, Redux, React Router, and more!</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
