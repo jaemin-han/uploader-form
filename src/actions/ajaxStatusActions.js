7ac528e888358758bb868eced654baa5e6d97390
jaemin.han <jaemin.han@nyu.edu>
2018-07-25 11:11:17 -0400

    handling async error and status

--- src/actions/ajaxStatusActions.js
+++ src/actions/ajaxStatusActions.js
@@ -3,3 +3,7 @@ import ActionTypes from './actionTypes';
 export function beginAjaxCall() {
     return {type: ActionTypes.BEGIN_AJAX_CALL};
 }
+
+export function ajaxCallError() {
+    return {type: ActionTypes.AJAX_CALL_ERROR};
+}
